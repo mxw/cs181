52c9e33ab769b5b984b68da7da766f3fe33684ff
Max Wang <mxawng@gmail.com>
2013-05-03 03:28:48 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+#
+# A Makefile.  For cleaning *.pyc files.
+#
+
+clean:
+	-rm -f *.pyc */*.pyc
+
+.PHONY: clean
---
+++ player.py
@@ -0,0 +1,16 @@
+import game_interface
+import random
+import time
+
+def get_move(view):
+    # Eat any plant we find.
+    hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT
+
+    # Choose a random direction.
+    if hasPlant:
+        for i in xrange(5):
+            print view.GetImage()
+
+    time.sleep(0.1)
+
+    return (random.randint(0, 4), hasPlant)
