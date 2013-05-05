6f246deea2e46158c15c41daf3b08a2ed2802195
Martin Camacho <camacho@college.harvard.edu>
2013-05-05 01:16:02 -0400

    starting work on smart walker

--- descender/player.py
+++ descender/player.py
@@ -3,7 +3,14 @@ import random
 import time
 
 
-BOUNDS = 20
+########################################
+# Initial Variables
+########################################
+
+BOUNDS = 25
+
+## Visited array
+
 seen = {}
 for x in range(-BOUNDS, BOUNDS+1):
     for y in range(-BOUNDS, BOUNDS+1):
@@ -11,18 +18,33 @@ for x in range(-BOUNDS, BOUNDS+1):
 
 seen[(0, 0)] = 'O'
 
-target = (-BOUNDS, -BOUNDS)
+## Density
 
-def next_target((X, Y)):
-    if Y == BOUNDS:
-        return (X + 1, -BOUNDS)
-    return (X, Y + 1)
 
+
+
+########################################
+# Move Logic
+########################################
+
+# choose the next move given current position and target.
+# assumes no noise
 def next_move((x, y), (tx, ty)):
     if x != tx:
         return game_interface.LEFT if tx < x else game_interface.RIGHT
     if y != ty:
         return game_interface.DOWN if ty < y else game_interface.UP
+
+
+
+
+def get_move(view):
+	return (random.randint(0, 3), False)
+
+########################################
+# Debugging/Printing Functions
+########################################
+
 
 def pl_chr(plant_info):
     if plant_info == game_interface.STATUS_UNKNOWN_PLANT:
@@ -32,39 +54,6 @@ def pl_chr(plant_info):
     if plant_info == game_interface.STATUS_POISONOUS_PLANT:
         return 'P'
     return ' '
-
-prev_life = 10000
-
-def get_move(view):
-    global seen, target
-    # Update based on life total
-    curr_life = view.GetLife()
-    #if prev_life - 10 == curr_life:
-
-
-    # Eat any plant we find.
-    hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT
-
-    # Choose a random direction.
-    # if hasPlant:
-    #     for i in xrange(5):
-    #         print view.GetImage()
-    #         print view.GetLife()
-
-    # time.sleep(0.1)
-
-    (X, Y) = (view.GetXPos(), view.GetYPos())
-
-    if abs(X) <= BOUNDS and abs(Y) <= BOUNDS and (X,Y) != (0,0):
-        seen[(X,Y)] = pl_chr(view.GetPlantInfo())
-
-    if target == (X, Y):
-        target = next_target(target)
-
-    #print target
-    # walk towards the target.
-    return (next_move((X, Y), target), hasPlant)
-
 
 def colorize(s):
     if s == 'N':
